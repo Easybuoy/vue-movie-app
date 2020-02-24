e34c34d2f7bc940979a208d80ccf1c7d363a02d6
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-02-24 17:30:09 +0100

    fix CORS issue

--- functions/getMovies.js
+++ functions/getMovies.js
@@ -3,11 +3,11 @@ const { makeRequest, headers } = require('../functions-utils');
 exports.handler = function(event, context, callback) {
   if (event.httpMethod !== 'POST') {
     // To enable CORS
-    return {
+    callback(null, {
       statusCode: 200, 
       headers,
       body: 'This was not a POST request!'
-    };
+    });
  }
 
   makeRequest(callback, 'GET');
