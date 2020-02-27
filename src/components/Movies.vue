b3fd7707a6fb0e48ae7a5fe1cd4b074c63fca6e1
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-02-27 17:12:24 +0100

    movies page

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,36 +1,35 @@
 <template>
   <div>
-     <v-container class="grey lighten-5">
-    <v-row no-gutters>
-      <v-card v-for="movie in movies" :key="movie.id" max-width="344" class="mx-auto mt-3">
-      <v-list-item>
-        <v-list-item-avatar color="grey"></v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title class="headline">{{movie.title}}</v-list-item-title>
-          <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+    <v-container class="grey lighten-5">
+      <v-row no-gutters>
+        <v-card v-for="movie in movies" :key="movie.id" max-width="344" class="mx-auto mt-3">
+          <v-list-item>
+            <!-- <v-list-item-avatar color="grey"></v-list-item-avatar> -->
+            <v-list-item-content>
+              <v-list-item-title class="headline">{{movie.title}}</v-list-item-title>
+              <!-- <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle> -->
+            </v-list-item-content>
+          </v-list-item>
 
-      <v-img  :src="'https://image.tmdb.org/t/p/w1280' + movie.backdrop_path" height="194"></v-img>
+          <v-img :src="'https://image.tmdb.org/t/p/w1280' + movie.backdrop_path" height="194"></v-img>
 
-      <v-card-text>{{movie.overview}}</v-card-text>
+          <v-card-text>{{movie.overview}}</v-card-text>
 
-      <v-card-actions>
-        <v-btn text color="deep-purple accent-4">View More</v-btn>
-        <!-- <v-btn text color="deep-purple accent-4">Bookmark</v-btn> -->
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-        <!-- <v-btn icon>
-          <v-icon>mdi-share-variant</v-icon>
-        </v-btn> -->
-      </v-card-actions>
-    </v-card>
-    </v-row>
-  </v-container>
-  
-    
+          <v-card-actions>
+            <v-btn text color="deep-purple accent-4">Details</v-btn>
+            <!-- <v-btn text color="deep-purple accent-4">Bookmark</v-btn> -->
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-row>
+    </v-container>
+
+    <div class="text-center">
+      <v-pagination v-model="page" :length="4" prev-icon="mdi-menu-left" next-icon="mdi-menu-right"></v-pagination>
+    </div>
   </div>
 </template>
 
@@ -42,7 +41,8 @@ const { API_BASE_URL } = configVariables;
 
 export default {
   data: () => ({
-    movies: []
+    movies: [],
+    page: 1
   }),
   methods: {
     getMovies() {
@@ -50,6 +50,7 @@ export default {
         .post(`${API_BASE_URL}/getPopularMovies`, {})
         .then(res => {
           this.movies = res.data.results;
+          this.page = res.data.page;
           console.log(res.data);
         })
         .catch(err => console.log(err));
