b2deebda981f6ac301fbf726076c8db0ab5d94d6
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-03-09 21:34:26 +0100

    add more items to dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,38 +1,39 @@
 <template>
   <div>
-    <v-carousel
-      v-model="model"
-      height="92vh"
-      cycle=""
-      interval="5000"
-      show-arrows-on-hover
-    >
+    <v-carousel v-model="model" height="92vh" cycle interval="5000" show-arrows-on-hover>
       <v-carousel-item
         v-for="item in items"
         :key="item.id"
         :src="'https://image.tmdb.org/t/p/w1280' + item.backdrop_path"
       >
         <v-row class="fill-height" align="center" justify="center">
-          <div class="display-3 text-uppercase white--text ">
+          <div class="display-3 text-uppercase white--text">
             <h3 class="mv-title">{{ item.title }}</h3>
           </div>
         </v-row>
       </v-carousel-item>
     </v-carousel>
+
+    <section class="movies mt-5" v-show="movies.length > 0">
+      <h2 class="deep-purple--text">Movies</h2>
+      <Movies preview="true" :movies="movies" />
+    </section>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import configVariables from '../config';
+import axios from "axios";
+import configVariables from "../config";
+import Movies from "./Movies/Movies";
 const { API_BASE_URL } = configVariables;
 
 export default {
   data() {
     return {
-      colors: ['primary', 'secondary', 'yellow darken-2', 'red', 'orange'],
+      colors: ["primary", "secondary", "yellow darken-2", "red", "orange"],
       model: 0,
-      items: []
+      items: [],
+      movies: []
     };
   },
   methods: {
@@ -46,16 +47,32 @@ export default {
           console.log(err);
         })
         .finally(() => (this.loading = false));
+    },
+    getMovies(page = 1) {
+      axios
+        .post(`${API_BASE_URL}/getNowPlayingMovies`, { page })
+        .then(res => {
+          this.movies = res.data.results;
+        })
+        .catch(err => {
+          console.log(err);
+          this.error = err;
+        })
+        .finally(() => (this.loading = false));
     }
   },
   beforeMount() {
     this.getTrending();
+    this.getMovies();
+  },
+  components: {
+    Movies
   }
 };
 </script>
 
 <style>
 .mv-title {
-  font-family: 'Concert One', cursive;
+  font-family: "Concert One", cursive;
 }
 </style>
